5756e951e1056de34f12c56ede8e1fb0da828630
k186 <k1868548@gmail.com>
2018-03-26 23:38:31 +0800

    好友对战

--- 资料/香阁儿/answer/pages/friendbattle/friendbattle.js
+++ 资料/香阁儿/answer/pages/friendbattle/friendbattle.js
@@ -1,66 +1,171 @@
 // pages/friendbattle/friendbattle.js
+import {battle} from '../../services/index'
+import utils from '../../common/utils/utils'
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    roomId: '',
+    level: '',
+    roomUsers: [],
+    roomOwner: '',
+    totalPoint: '',
+    userId: '',
+    subject: {},
+    subjectCount: 0,
+    Timer: null,
+    isAnswered: false,
+    result: {},
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    this.setData({
+      level: options.level || 1,
+      roomId: options.roomId || ''
+    });
+    this.initPage();
   },
-
   /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
+   * 初始化
+   * */
+  initPage () {
+    let UserInfo = utils.getStorageSync('userInfo') || {};
+    let token = utils.getValueByPath(UserInfo, 'token');
+    if (!token) {
+      console.log('获取用户信息失败了')
+      return
+    }
+    this.setData({
+      userId: UserInfo.user.id
+    });
+    battle.PVF_connect(this.data.level, token, this.data.roomId, () => {
+      console.log('好友对战连接成功:----------------------');
+      this.getMessage();
+    })
   },
-
   /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
+   * 监听信息
+   * */
+  getMessage () {
+    battle.PVF_onMessage((res) => {
+      console.log('好友对战接收到消息了:----------------------');
+      if (res.type == 1) {
+        this.initRoom(res);
+      }
+      if (res.type == 2) {
+        console.log('好友连接上了:-----------------------------');
+        this.updateRoom(res);
+      }
+    })
+  },
+  /**
+   * 初始化房间信息
+   * */
+  initRoom (res) {
+    console.log('房间信息:-----------------');
+    console.log(res);
+    console.log('房间信息:-----------------');
+    console.log(res.roomId)
+    this.setData({
+      roomUsers: res.roomUsers || [],
+      roomOwner: res.userId || '',
+      roomId: res.roomId || '',
+      totalPoint: res.totlePoint || ''
+    })
+  },
+  /**
+   * 更新房间信息
+   * */
+  updateRoom (res) {
+    console.log('房间信息更新:-----------------');
+    console.log(res);
+    console.log('房间信息更新:-----------------');
+    this.setData({
+      roomUsers: res.roomUsers || [],
+    })
+  },
+  /**
+   * 发送消息
+   * */
+  sendMessage (data) {
+    battle.PVF_send(data);
+  },
+  /**
+   * 开始对战
+   * */
+  startBattle () {
+    this.sendMessage({"type": 4});
+    this.animationEvt('start', () => {
+
+    })
+    this.getSubject();
+  },
+  /**
+   * 场景动画
+   * */
+  animationEvt (type, callback) {
+    let room = wx.wx.createAnimation({
+      duration: 500,
+      timingFunction: 'ease',
+    });
+    if (type == 'reset') {
+      //所有动画还原
+    }
+    if (type == 'start') {
+      //隐藏房间
+      room.opacity(0).setp();
+      this.setData({
+        roomAniData: room.export()
+      })
+      setTimeout(() => {
+        callback && callback();
+      }, 500)
+    }
+  },
+  /**
+   * 获取题目
+   * */
+  getSubject () {
+    this.sendMessage({type: 1, subjectOffset: this.data.subjectCount + 1})
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
-  },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
   },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+    return {
+      title: '等你来战',
+      path: '/pages/friendbattle/friendbattle?roomId=' + this.data.roomId,
+      //image: '',
+      success: function (res) {
+        // utils.showToast({
+        //   title: '邀请已发送!'
+        // })
+      },
+      fail: function (res) {
+        // utils.showToast({
+        //   title: '邀请失败!'
+        // })
+      }
+    }
   }
 })
--- 资料/香阁儿/answer/services/battle/battle.js
+++ 资料/香阁儿/answer/services/battle/battle.js
@@ -5,13 +5,17 @@ const app = getApp();
 export default {
   apiList: {
     PVP: app.HOST_SOCKET + '/singleFightAgainst',
+    PVF: app.HOST_SOCKET + '/friendFightAgainst'
   },
   PVP_messageList: [],
-  PVA_messageList: [],
   PVP_isConnect: false,
   PVP_socket: {},
+  PVA_messageList: [],
   PVA_socket: {},
   PVA_isConnect: false,
+  PVF_messageList: [],
+  PVF_socket: {},
+  PVF_isConnect: false,
   PVP_connect (danGrading, callback) {
     let UserInfo = utils.getStorageSync('userInfo') || {};
     let token = utils.getValueByPath(UserInfo, 'token');
@@ -107,6 +111,51 @@ export default {
     if (this.PVA_isConnect) {
       console.log('PVP close')
       this.PVA_socket.close({})
+    }
+  },
+  PVF_connect (danGrading, token, roomId, callback) {
+    this.PVF_socket = ajax.connectSocket(this.apiList.PVF, {token: token, roomId: roomId, danGrading: danGrading || 1});
+    this.PVF_socket.onOpen(res => {
+      this.PVF_isConnect = true;
+      callback(res);
+    })
+  },
+  PVF_send (data) {
+    var messageList = this.PVF_messageList;
+    messageList.push(data);
+    if (this.PVF_isConnect) {
+      for (var i = 0; i < this.PVF_messageList.length; i++) {
+        let data = JSON.stringify(this.PVF_messageList[i]);
+        this.PVF_socket.send({
+          data: data
+        })
+        this.PVF_messageList = [];
+      }
+    }
+  },
+  PVF_onMessage (callback) {
+    this.PVF_socket.onMessage(res => {
+      var res = (typeof res.data == 'string' ? JSON.parse(res.data) : res.data) || null;
+      if (res.code != '0000') {
+        utils.showToast({
+          title: res.msg || res.message || '',
+        });
+        return
+      }
+      callback(res.data);
+    })
+  },
+  PVF_onError (callback) {
+    this.PVF_socket.onError(res => {
+      utils.showToast({
+        title: '连接错误！',
+      });
+    })
+  },
+  PVF_close () {
+    if (this.PVF_isConnect) {
+      console.log('PVF close')
+      this.PVF_socket.close({})
     }
   },
 }
