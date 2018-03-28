337d9348f51ac977f87b36adb688c9c65e77c563
xiong.yi <xiong.yi@dmall.com>
2018-03-28 18:11:30 +0800

    rank列表

--- 资料/香阁儿/answer/pages/rank/rank.js
+++ 资料/香阁儿/answer/pages/rank/rank.js
@@ -1,74 +1,132 @@
 // pages/rank/rank.js
 import utils from '../../common/utils/utils'
+import {rank} from '../../services/index'
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    active:false
+    active: false,
+    rankData: {},
+    scrollToView: 0,
+    skills: [{}, {}, {}, {}],
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  setTimeout(()=>{
-    this.setData({
-      active:true
-    })
-  },5000)
+    this.updateRank();
   },
+  /**
+   * 更新rank
+   * */
+  updateRank() {
+    rank.getRank((res) => {
+      if (res.code !== '0000') {
+        return
+      }
+      this.filterData(res.data)
 
-  rankBattleEvt(e){
-    utils.navigateTo('../battleone/battleone')
+    });
+  },
+  filterData(data) {
+    let scrollToView = 0
+    for (let i = 0; i < data.danGradingList.length; i++) {
+      let item = data.danGradingList[i];
+      if (!item.deblock && i != 0) {
+        scrollToView = i - 1
+        break
+      }
+    }
+    //用户技能处理
+    let skill = [];
+    let skills = data.user.skillTypes || [];
+    for (let i = 0; i < 4; i++) {
+      skill.push({
+        active: false
+      })
+      for (let k = 0; k < skills.length; k++) {
+        if (i + 1 == skills[k]) {
+          skill[i].active = true
+        }
+      }
+    }
+    this.setData({
+      skills: skill
+    })
+
+    let userCurrentLevel = data.user.danGradingLevel;
+    let count = data.user.danGradingProcess;
+    for (let i = 0; i < data.danGradingList.length; i++) {
+      let item = data.danGradingList[i];
+      item['processCountArr'] = [];
+      for (var k = item.processCount; k >= 0; k--) {
+        item['processCountArr'].push({active: false})
+      }
+      if (item.level < userCurrentLevel) {
+        item['processCountArr'] = item['processCountArr'].map((el) => {
+          el.active = true
+          return el
+        })
+      }
+      this.setData({
+        rankData: data,
+        scrollToView: scrollToView
+      })
+      if (item.level == userCurrentLevel) {
+        for (let y = 0; y < item['processCountArr'].length; y++) {
+          if (y  < count) {
+            setTimeout(() => {
+              item['processCountArr'][y].active = true;
+              this.setData({
+                rankData: data
+              })
+            }, 1000)
+          }
+        }
+      }
+    }
+  },
+  rankBattleEvt(e) {
+    let level = e.currentTarget.dataset.level || 1
+    utils.navigateTo('../battleone/battleone', {level: level})
   },
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   }
 })
--- 资料/香阁儿/answer/services/rank/rank.js
+++ 资料/香阁儿/answer/services/rank/rank.js
@@ -2,25 +2,12 @@ import ajax from '../../common/ajax/ajax';
 import utils from '../../common/utils/utils';
 
 const app = getApp();
-let handle;
-
-///user/rank/list 用户好友榜
-///rank/list  世界排行榜
-handle = {
-  getRank : function() {
-    ajax.request( {
-      url : app.HOST_AJAX + '/user/rank/list',
-      callback : function( res ) {
-      	console.log( res );
-      }
-    }, function() {
-      
-    } );
-  },
-  getGlobalRank : function() {
-
+//app/danGrading/list  断位列表
+export default {
+  getRank: function (callback) {
+    ajax.request({
+      url: app.HOST_AJAX + '/app/danGrading/list',
+      callback
+    });
   }
 }
-//todo 调用报错 零时注释掉
-//handle.getRank();
-export default handle;
