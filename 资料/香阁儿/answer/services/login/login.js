22d7871f00b04b1ba6bd897a7b8629515db3459f
k186 <k1868548@gmail.com>
2018-03-31 16:47:07 +0800

    登录

--- 资料/香阁儿/answer/services/login/login.js
+++ 资料/香阁儿/answer/services/login/login.js
@@ -6,38 +6,63 @@ export default {
   apiList: {
     login: app.HOST_AJAX + '/login'
   },
-  login(callback) {
-    let that = this;
-    wx.login({
-      success(res1) {
-        wx.getUserInfo({
-          lang: 'zh_CN',
-          success(res2) {
-            console.log({code: res1.code, iv: res2.iv, encryptedData: res2.encryptedData})
-            ajax.request({
-              url: that.apiList.login,
-              data: {code: res1.code, iv: res2.iv, encryptedData: res2.encryptedData},
-              callback(res3) {
-                //本地信息缓存
-                console.warn(res3.data.token)
-                utils.setStorageSync('userInfo', res3.data);
-                callback&&callback(true)
-              }
-            })
-          },
-          fail(res) {
-            utils.showAction('请开启授权,否则无法正常体验小程序', (res) => {
-              if (res) {
-                wx.openSetting({});
-              }
-            });
-            callback&&callback(false)
-          }
-        })
+  checkSingleAuthorize (scopeItem, callback) {
+    wx.getSetting({
+      success: (response) => {
+        if (response.authSetting[scopeItem]) {
+          callback && callback(true);
+        } else if (response.authSetting[scopeItem] == undefined) {
+          wx.authorize({
+            scope: scopeItem,
+            success () {
+              callback && callback(true);
+            },
+            fail () {
+              callback && callback(false);
+            }
+          })
+        } else {
+          callback && callback(false);
+        }
       }
-    });
+    })
   },
-  isLogin(callback) {
+  login (callback) {
+    let that = this;
+    this.checkSingleAuthorize('scope.userInfo', (res) => {
+      if (!res) {
+        utils.showAction('需要开启授权才能进行游戏哦~', res => {
+          if (res) {
+            wx.openSetting()
+          }
+        });
+        return
+      }
+      wx.login({
+        success (res1) {
+          wx.getUserInfo({
+            lang: 'zh_CN',
+            success (res2) {
+              wx.showToast({
+                title: '登录中...',
+                icon: 'loading'
+              });
+              ajax.request({
+                url: that.apiList.login,
+                data: {code: res1.code, iv: res2.iv, encryptedData: res2.encryptedData},
+                callback (res3) {
+                  utils.setStorageSync('userInfo', res3.data);
+                  callback && callback(true)
+                }
+              })
+            }
+          })
+        }
+      });
+    })
+
+  },
+  isLogin (callback) {
     let UserInfo = this.getLoginInfo();
     if (UserInfo) {
       callback(true)
@@ -45,7 +70,7 @@ export default {
       callback(false)
     }
   },
-  getLoginInfo() {
+  getLoginInfo () {
     let userInfo = utils.getStorageSync('userInfo') || null;
     return userInfo
   }
