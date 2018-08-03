2d78477d2338cb8e80cbcac4876845f430464ec3
jun.li <jun.li@dmall.com>
2018-08-03 15:16:20 +0800

    党建小程序 修改

---
+++ dangjian/newmp/pages/admin/admin.js
@@ -0,0 +1,11 @@
+Page({
+    data : {
+        markers : [{
+            id : 0,
+            longitude : 104.00453,
+            latitude : 30.63008,
+        }]
+    }
+});
+
+
--- dangjian/newmp/pages/detail/detail.js
+++ dangjian/newmp/pages/detail/detail.js
@@ -1,1 +1,13 @@
-Page({});
+var pageParam;
+
+Page({
+    onLoad : function( p ) {
+        pageParam = p || {};
+    },
+
+    onReady : function() {
+        this.setData( {
+            id : pageParam.id
+        } );
+    }
+});
