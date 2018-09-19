579e21ceb3944c67e1fd494d266386af040e1a16
ypanda <k1868548@gmail.com>
2018-09-19 18:02:24 +0800

    checkout

--- bcup/src/widgets/toolbar/toolbar.vue
+++ bcup/src/widgets/toolbar/toolbar.vue
@@ -1,15 +1,15 @@
 <template>
     <div class="toolbar clearfix">
-        <router-link :to="{ path : '/' }" class="home">H</router-link>
+        <router-link :to="{ path : '/' }" class="home">首页</router-link>
         <div class="cont">
-            <div class="btn" @click="toggleService">services</div>
-            <router-link :to="{ path : '/checkout', query : { skuId : skuId } }" class="btn primary">now</router-link>
+            <div class="btn" @click="toggleService">咨询客服 </div>
+            <router-link :to="{ path : '/checkout', query : { skuId : skuId } }" class="btn primary">立即购买</router-link>
         </div>
         <transition name="fade">
             <div v-if="showService" class="pop">
                 <div class="list">
-                    <div class="item">call</div>
-                    <div class="item">center</div>
+                  <a class="item" :href="'tel:'+{}">拨打电话</a>
+                  <router-link tag="div" :to="{ path : '/servicecenter'}"  class="item">服务中心</router-link>
                 </div>
                 <div class="shim" @click="toggleService"></div>
             </div>
@@ -24,13 +24,13 @@
     .cont .btn.primary { background-color : #efefef; transition : all 0.5s; }
     .pop { height : 100%; width : 100%; position: fixed; top : -44px; left : 0; z-index: 10000; overflow: hidden; }
     .pop .list { position: absolute; bottom : 0; left : 0; width : 100%; z-index: 1; transform : translate( 0, 0 ); transition : all 0.3s;  }
-    .pop .list .item { background-color: #fff; border-bottom : solid 1px #f0f0f0; padding : 0 10px; height : 60px; line-height: 60px; }
+    .pop .list .item {display: block; background-color: #fff; border-bottom : solid 1px #f0f0f0; padding : 0 10px; height : 60px; line-height: 60px; }
     .pop .list .item:last-child { border-bottom : 0; }
     .pop .shim { background-color: #000; opacity: 0.2; width : 100%; height : 100%; position: absolute; top : 0; left : 0; transition : all 0.5s; }
 
     .fade-enter-active, .fade-leave-active { transition : all 0.5s; }
     .fade-enter .shim, .fade-leave-to .shim { opacity: 0; }
-    .fade-enter .list, .fade-leave-to .list {  transform : translate( 0, 120px ); }    
+    .fade-enter .list, .fade-leave-to .list {  transform : translate( 0, 120px ); }
 </style>
 
 <script>
@@ -40,11 +40,11 @@
             return {
                 showService : false
             };
-        },     
+        },
         methods : {
             toggleService : function() {
                 this.showService = !this.showService;
             }
-        }       
+        }
     }
 </script>
